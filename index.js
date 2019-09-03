6f1040db6486609bc9ee122771a68d114d9db72e
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-09-03 08:18:56 -0400

    add moment and timestamp

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const moment = require('moment');
+
 exports.handler = async (event, context) => {
   const body = {
-    key: process.env.API_KEY
+    key: process.env.API_KEY,
+    timestamp: moment().unix()
   };
 
   return {
